6ca7c9d7414080eea195917d906db42760bf2265
Chelsea Troy <chelsea.dommert@gmail.com>
2019-05-01 19:33:40 -0500

    Initial Commit

---
+++ SplitViewDemo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SplitViewDemo
+//
+//  Created by Chelsea Troy on 5/1/19.
+//  Copyright © 2019 Chelsea Troy. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
